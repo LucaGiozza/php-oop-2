bf6b0f6328eef9a2b45ca89dfce78478b31f4241
Luca Giozza <luca.giozza@gmail.com>
2021-09-02 15:36:20 +0200

    scheletro dell'esercizio con nome-cognome e sconto

---
+++ Utente.php
@@ -0,0 +1,43 @@
+<!-- Ciao ragazzi, esercizio di oggi: nome repo: php-oop-2
+Oggi pomeriggio provate ad immaginare quali sono le classi necessarie per creare uno shop online; ad esempio,
+ ci saranno sicuramente dei prodotti da acquistare e degli utenti che fanno shopping. Strutturare le classi gestendo
+  l'ereditarietà dove necessario; ad esempio ci potrebbero essere degli utenti premium che hanno diritto a degli sconti esclusivi,
+   oppure diverse tipologie di prodotti.
+Provate a far interagire tra di loro gli oggetti: ad esempio, l'utente dello shop inserisce una carta di credito...
+ $c = new CreditCard(..); $user->insertCreditCard($c); -->
+
+
+ <?php
+
+ class Utente {
+   public $name;
+   public $surname;
+   public $sconto = 0;
+   public $conto_da_pagare;
+
+   function __construct($_name, $_surname){
+       $this->name = $_name;
+       $this->surname = $_surname;
+
+   }
+
+   public function getNameSurname(){
+     return $this->name . ' ' . $this->surname;
+     
+   }
+
+   public function setSconto($eta){
+     if($eta > 60){
+       $this->sconto = 30;
+
+     }
+
+
+   }
+
+   public function getSconto(){
+      return $this->sconto;
+   }
+
+
+ }
